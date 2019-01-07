0cd367322e9d9f1bdb849baba95253d9ada33e00
alau7 <alau7@gatech.edu>
2019-01-07 00:28:37 -0800

    add basic server functionality

--- web-app/src/app/home-view/home-view.component.ts
+++ web-app/src/app/home-view/home-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-home-view',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
+    this.httpClient.get('https://superfacilapp.appspot.com/')
+    .subscribe(data => {
+      console.log(data);
+    })
   }
 
 }
