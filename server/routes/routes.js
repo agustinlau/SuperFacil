4e2fed1360e65c5e6c991f486ce25d9b4a080321
alau7 <alau7@gatech.edu>
2019-01-14 23:35:50 -0800

    add gcloud datastore dependency and .gitignore file to server

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,13 @@
+const Datastore = require('@google-cloud/datastore');
+
+// Instantiate a datastore client
+const datastore = Datastore();
+
+
+
 var appRouter = function (app) {
     app.get("/", function(req, res) {
+      console.log(datastore);
       res.status(200).send("Welcome to our restful API");
     });
 }
