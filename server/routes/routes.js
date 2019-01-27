c03e29b274eced442b40ad76ff14536c82c4ab38
alau7 <alau7@gatech.edu>
2019-01-26 19:07:55 -0800

    add test method to add to datastore gcp database

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,15 +1,50 @@
 const Datastore = require('@google-cloud/datastore');
 
 // Instantiate a datastore client
-const datastore = Datastore();
+const datastore = new Datastore({
+  projectId: 'superfacilapp',
+  // keyFilename: '/path/to/keyfile.json'
+});
 
 
 
 var appRouter = function (app) {
     app.get("/", function(req, res) {
-      console.log(datastore);
       res.status(200).send("Welcome to our restful API");
     });
+
+    app.get("/test", function(req, res) {
+      console.log(req);
+      const key = datastore.key('test');
+      const entity = {
+        key: key,
+        data: [
+          {
+            name: 'created',
+            value: new Date().toJSON(),
+          },
+          {
+            name: 'description',
+            value: 'description test',
+            excludeFromIndexes: true,
+          },
+          {
+            name: 'done',
+            value: false,
+          },
+        ],
+      };
+
+      datastore.save(entity)
+      .then(() => {
+        console.log(`Task ${key.id} created successfully.`);
+      })
+      .catch(err => {
+        console.error('ERROR:', err);
+      });
+      res.status(200).send("Welcome to our restful API");
+    });
+    
 }
   
 module.exports = appRouter;
